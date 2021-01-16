6b7ccd44e0d575c0cd095a3b594f81e3d9911fb8
vproskurin <vladimir.proskurin@gmail.com>
2021-01-16 11:58:24 -0800

    gatsby linkin

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "gatsby";
+
+export default () => {
+  return (
+    <>
+      <h1>About me</h1>
+      <p>This is a website for test</p>
+      <Link to="/">&larr; back to home</Link>
+    </>
+  );
+};
