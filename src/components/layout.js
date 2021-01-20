69297385414e19a3c8d7adb384f5776a871d62f1
vproskurin <vladimir.proskurin@gmail.com>
2021-01-19 23:52:14 -0800

    gatsby jss02

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,31 @@
 import React from "react";
-import { Global,css } from "@emotion/core";
+import { Global, css } from "@emotion/core";
 
-const Layout = ({children}) => {
-    return (
-        <>
-        <Global styles={css`
-        * {
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Global
+        styles={css`
+          * {
             box-sizing: border-box;
-        }
-        
-        * + * {
-            margin-top: 1rem;
-        }
+          }
 
-        html, body {
+          * + * {
+            margin-top: 1rem;
+          }
+
+          html,
+          body {
             margin: 0;
             color: #555;
-        }
-        `} />
-        </>
-    )
-}
+            font-family: --apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+              Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue",
+              sans-serif;
+            font-size: 18p;
+            line-height: 1.4;
+          }
+        `}
+      />
+    </>
+  );
+};
