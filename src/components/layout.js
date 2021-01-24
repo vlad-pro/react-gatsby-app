f9b8017ef76fc0e8dcc2bab0f97f5b90bd291de7
vproskurin <vladimir.proskurin@gmail.com>
2021-01-24 13:49:57 -0800

    gatsby helmet&GQP02

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Global, css } from "@emotion/react";
-import Helmet  from "react-helmet";
+import Helmet from "react-helmet";
 import Header from "./header";
+import useSiteMetadata from "./hooks/use-sitemetadata";
 
 const Layout = ({ children }) => {
+  const { title, description } = useSiteMetadata();
   return (
     <>
       <Global
@@ -47,13 +49,17 @@ const Layout = ({ children }) => {
           }
         `}
       />
-      <Header/>
+      <Helmet>
+        <html lang="en" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Helmet>
+      <Header />
       <main
         css={css`
           margin: 2rem auto 4rem;
           max-width: 90vw;
           width: 550px;
-
         `}
       >
         {children}
