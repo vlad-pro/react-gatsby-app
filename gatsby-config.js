2ffe662bbca3aba27218ccf7e18210fdd934a602
vproskurin <vladimir.proskurin@gmail.com>
2021-01-25 20:22:17 -0800

    gatsby MDX01

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,30 @@ module.exports = {
     title: "React Gatsby App",
     description: "A site built while learing Gatsby",
   },
-  plugins: ["gatsby-plugin-emotion", "gatsby-plugin-react-helmet"],
+  plugins: [
+    "gatsby-plugin-emotion",
+    "gatsby-plugin-react-helmet",
+    // {
+    //   resolve: `gatsby-source-filesystem`,
+    //   options: {
+    //     name: `pages`,
+    //     path: `${__dirname}/src/pages/`,
+    //   },
+    // },
+    // {
+    //   resolve: "gatsby-plugin-page-creator",
+    //   options: {
+    //     path: `${__dirname}/src/posts`,
+    //   },
+    // },
+    
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: require.resolve("./src/components/layout.js"),
+        },
+      },
+    },
+  ],
 };
